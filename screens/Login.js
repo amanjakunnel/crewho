29786502bdaf2a9b52a311f810544b1dcc76edb8
Nitin Arul <nitinarul68@gmail.com>
2020-08-04 18:51:19 +0530

    my changes

--- screens/Login.js
+++ screens/Login.js
@@ -1,59 +1,94 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {
+	View,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from 'react-native';
 import globalStyles from '../shared/Global';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
 
 const reviewSchema = yup.object({
-    Username: yup.string().required().test('is_user', 'Username is not registered', (val) => {
-        return val == 'Username';
-    }),
-    Password: yup.string().required().test('is_pass', 'Password is incorrect', (val) => {
-        return val == 'Password';
-    }),
-})
+	Username: yup
+		.string()
+		.required()
+		.test('is_user', 'Username is not registered', (val) => {
+			return val == 'Username';
+		}),
+	Password: yup
+		.string()
+		.required()
+		.test('is_pass', 'Password is incorrect', (val) => {
+			return val == 'Password';
+		}),
+});
 
-export default function Login({navigation}) {
-    return (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={globalStyles.app}>
-                <Text style={globalStyles.header}>Login</Text>
-                <Formik 
-                    initialValues={{ Username: '', Password: ''}} 
-                    //validationSchema={reviewSchema}
-                    onSubmit={(values) => {
-                            axios
-                                .post('127.0.0.1:5000/users/login', {
-                                    email: values.Username,
-                                    password: values.Password,
-                                })
-                                .then(res => {
-                                    if(res == 1) {
-                                        navigation.navigate('Home');
-                                    }
-                            })
-                    }}
-                >
-                    {(props) => (
-                        <View>
-                            <TextInput style={globalStyles.input} placeholder='Username' placeholderTextColor='white' onChangeText={props.handleChange('Username')} value={props.values.Username} />
-                            <Text style={globalStyles.error}>{ props.touched.Username && props.errors.Username }</Text>
-                            <TextInput style={globalStyles.input} placeholder='Password' placeholderTextColor='white' onChangeText={props.handleChange('Password')} value={props.values.Password} />
-                            <Text style={globalStyles.error}>{ props.touched.Password && props.errors.Password }</Text>
-                            <TouchableOpacity style={globalStyles.button} onPress={props.handleSubmit}>
-                                <Text style={globalStyles.buttonText}>Submit</Text>
-                            </TouchableOpacity>
-                        </View>
-                    )}
-                </Formik>
-                <View style={globalStyles.tap}>
-                    <Text style={globalStyles.tapper}>New user? Click </Text>
-                    <TouchableOpacity onPress={() => navigation.navigate('Signup1')}>
-                        <Text style={globalStyles.tappable}>Signup</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </TouchableWithoutFeedback>
-    )
+export default function Login({ navigation }) {
+	return (
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<View style={globalStyles.app}>
+				<Text style={globalStyles.header}>Login</Text>
+				<Formik
+					initialValues={{ Username: '', Password: '' }}
+					//validationSchema={reviewSchema}
+					onSubmit={(values) => {
+						axios
+							.post('http://192.168.1.122:5000/users/login', {
+								email: values.Username,
+								password: values.Password,
+							})
+							.then((res) => {
+								if (res.status == 200) {
+									navigation.navigate('Home');
+								}
+							})
+							.catch((err) => {
+								console.log(err);
+							});
+					}}
+				>
+					{(props) => (
+						<View>
+							<TextInput
+								style={globalStyles.input}
+								placeholder='Username'
+								placeholderTextColor='white'
+								onChangeText={props.handleChange('Username')}
+								value={props.values.Username}
+							/>
+							<Text style={globalStyles.error}>
+								{props.touched.Username && props.errors.Username}
+							</Text>
+							<TextInput
+								style={globalStyles.input}
+								placeholder='Password'
+								placeholderTextColor='white'
+								onChangeText={props.handleChange('Password')}
+								value={props.values.Password}
+							/>
+							<Text style={globalStyles.error}>
+								{props.touched.Password && props.errors.Password}
+							</Text>
+							<TouchableOpacity
+								style={globalStyles.button}
+								onPress={props.handleSubmit}
+							>
+								<Text style={globalStyles.buttonText}>Submit</Text>
+							</TouchableOpacity>
+						</View>
+					)}
+				</Formik>
+				<View style={globalStyles.tap}>
+					<Text style={globalStyles.tapper}>New user? Click </Text>
+					<TouchableOpacity onPress={() => navigation.navigate('Signup1')}>
+						<Text style={globalStyles.tappable}>Signup</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+		</TouchableWithoutFeedback>
+	);
 }
--- screens/Signup4.js
+++ screens/Signup4.js
@@ -1,57 +1,110 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {
+	View,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from 'react-native';
 import globalStyles from '../shared/Global';
 import { Formik } from 'formik';
 import * as yup from 'yup';
-import axios from 'axios'
+import axios from 'axios';
 
 const reviewSchema = yup.object({
-    Locality: yup.string().required(),
-    City: yup.string().required(),
-    State: yup.string().required(),
-    Country: yup.string().required()
-})
+	locality: yup.string().required(),
+	city: yup.string().required(),
+	state: yup.string().required(),
+	Country: yup.string().required(),
+});
 
-export default function Signup_4({navigation}) {
-    const Email = navigation.getParam('Email')
-    const Password = navigation.getParam('Password')
-    const Username = navigation.getParam('Username')
-    const Phone = navigation.getParam('Phone')
-    const Name = navigation.getParam('Name')
-    const Skills = navigation.getParam('Skills')
-    const Gender = navigation.getParam('Gender')
-    return (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={globalStyles.app}>
-                <Text style={globalStyles.header}>Signup</Text>
-                <Formik
-                    initialValues={{ Locality: '', City: '', State: '', Country: ''}} 
-                    validationSchema={reviewSchema}
-                    onSubmit={(values) => {
-                        axios
-                                .post('127.0.0.1:5000/users/login', { ...values, email, password, username, phone_no, name, skills, gender })
-                                .then(res => {
-                                        navigation.navigate('Home');
-                            })
-                    }}
-                >
-                    {(props) => (
-                        <View>
-                            <TextInput style={globalStyles.input} placeholder='Locality' placeholderTextColor='white' onChangeText={props.handleChange('Locality')} value={props.values.Locality} />
-                            <Text style={globalStyles.error}>{ props.touched.Locality && props.errors.Locality }</Text>
-                            <TextInput style={globalStyles.input} placeholder='City' placeholderTextColor='white' onChangeText={props.handleChange('City')} value={props.values.City} />
-                            <Text style={globalStyles.error}>{ props.touched.City && props.errors.City }</Text>
-                            <TextInput style={globalStyles.input} placeholder='State' placeholderTextColor='white' onChangeText={props.handleChange('State')} value={props.values.State} />
-                            <Text style={globalStyles.error}>{ props.touched.State && props.errors.State }</Text>
-                            <TextInput style={globalStyles.input} placeholder='Country' placeholderTextColor='white' onChangeText={props.handleChange('Country')} value={props.values.Country} />
-                            <Text style={globalStyles.error}>{ props.touched.Country && props.errors.Country }</Text>
-                            <TouchableOpacity style={globalStyles.button}>
-                                <Text style={globalStyles.buttonText} onPress={props.handleSubmit}>Continue</Text>
-                            </TouchableOpacity>
-                        </View>
-                    )}
-                </Formik>
-            </View>
-        </TouchableWithoutFeedback>
-    )
+export default function Signup_4({ navigation }) {
+	const email = navigation.getParam('Email');
+	const password = navigation.getParam('Password');
+	const username = navigation.getParam('Username');
+	const phone_no = navigation.getParam('Phone');
+	const name = navigation.getParam('Name');
+	const skills = navigation.getParam('Skills');
+	const gender = navigation.getParam('Gender');
+	return (
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<View style={globalStyles.app}>
+				<Text style={globalStyles.header}>Signup</Text>
+				<Formik
+					initialValues={{ locality: '', city: '', state: '', country: '' }}
+					validationSchema={reviewSchema}
+					onSubmit={(values) => {
+						axios
+							.post('http://192.168.1.122:5000/users/signup', {
+								...values,
+								email,
+								password,
+								username,
+								phone_no,
+								name,
+								skills,
+								gender,
+							})
+							.then((res) => {
+								navigation.navigate('Home');
+							});
+					}}
+				>
+					{(props) => (
+						<View>
+							<TextInput
+								style={globalStyles.input}
+								placeholder='Locality'
+								placeholderTextColor='white'
+								onChangeText={props.handleChange('Locality')}
+								value={props.values.locality}
+							/>
+							<Text style={globalStyles.error}>
+								{props.touched.locality && props.errors.locality}
+							</Text>
+							<TextInput
+								style={globalStyles.input}
+								placeholder='City'
+								placeholderTextColor='white'
+								onChangeText={props.handleChange('City')}
+								value={props.values.City}
+							/>
+							<Text style={globalStyles.error}>
+								{props.touched.City && props.errors.City}
+							</Text>
+							<TextInput
+								style={globalStyles.input}
+								placeholder='State'
+								placeholderTextColor='white'
+								onChangeText={props.handleChange('State')}
+								value={props.values.State}
+							/>
+							<Text style={globalStyles.error}>
+								{props.touched.State && props.errors.State}
+							</Text>
+							<TextInput
+								style={globalStyles.input}
+								placeholder='Country'
+								placeholderTextColor='white'
+								onChangeText={props.handleChange('Country')}
+								value={props.values.Country}
+							/>
+							<Text style={globalStyles.error}>
+								{props.touched.Country && props.errors.Country}
+							</Text>
+							<TouchableOpacity style={globalStyles.button}>
+								<Text
+									style={globalStyles.buttonText}
+									onPress={props.handleSubmit}
+								>
+									Continue
+								</Text>
+							</TouchableOpacity>
+						</View>
+					)}
+				</Formik>
+			</View>
+		</TouchableWithoutFeedback>
+	);
 }
